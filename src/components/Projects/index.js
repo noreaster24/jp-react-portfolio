16aff0f8dd837bf60cbeb62e0eb24ffc660c6a1c
Joe Porrazzo <joepa2624@me.com>
2021-07-31 15:34:46 -0700

    projects bug fixes

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -9,7 +9,7 @@ function Projects() {
                 <li key={i}>
                     <h2 class="project-title">{p.title}</h2>
                     <p class="project-info">{p.description}</p>
-                    <img class="project-img" src={p.image} alt={p.title}/>
+                    <img src={p.image} alt=''/>
                     <p class="project-tags">{p.tags.map(p.tag)}</p>
                     <a href={p.source} class="project-repo">Code</a> 
                     <a href={p.visit} class="project-link">Source</a>
