d7c69327b91caa6fd1cc2bf1cdcaeb6e0fa2b869
Joe Porrazzo <joepa2624@me.com>
2021-08-02 07:20:19 -0700

    bio section updates

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -4,11 +4,29 @@ import workExperienceImage from '../../assets/images/porrazzo-resume.png';
 function Work() {
 
     return (
-        <div className="bio-container">
-            <h1>Work Experience</h1>
-            <img className="bio-img" src={workExperienceImage} alt="JP Work Experience" />
-            <button onClick="https://drive.google.com/file/d/0B_lMIZ5YdLI_NjBRRGc3NDdlejA/view?usp=sharing&resourcekey=0-e1z-Gcx1ql8z9Fw9jVUshQ" className="view-btn">View Resume</button>
-        </div>
+        <section className="experience">
+            <div className="bio-container">
+                <h1>Work Experience</h1>
+                <img className="bio-img" src={workExperienceImage} alt="JP Work Experience" />
+                <button onClick="https://drive.google.com/file/d/0B_lMIZ5YdLI_NjBRRGc3NDdlejA/view?usp=sharing&resourcekey=0-e1z-Gcx1ql8z9Fw9jVUshQ" className="view-btn">View Resume</button>
+            </div>
+            <div className="skills">
+                <ul className="skills-list">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>Sequelize</li>
+                    <li>MySQL</li>
+                    <li>MongoDB</li>
+                    <li>React</li>
+                    <li>Git/GitHub</li>
+                    <li>Heroku</li>
+                </ul>
+            </div>
+        </section>
+
     );
 }
 
