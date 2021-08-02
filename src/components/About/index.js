111b2edbbcd3fe7dd258f39293be44e7a2a9c2e6
Joe Porrazzo <joepa2624@me.com>
2021-08-01 20:49:07 -0700

    finished About Me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import coverImage from '../../assets/images/mb-hero.webp';
+import coverImage from '../../assets/images/JP-main.jpeg';
+import heroImage from '../../assets/images/mb-hero.webp'
 
 
 function About() {
     return (
-        <section id="about-me" className="my-5">
-            <h1 id="about">Who am I?</h1>
-            <img src={coverImage} className="my-2" style={{ width: "25%" }} alt="cover" />
-            <p id="salutation">
-                Hi!  
-            </p>
-            <p id="profile-overview">
-                I'm a product manager that focuses on new product development, and an aspiring full-stack web developer. I love technology and I'm always looking to learn new ways to do things.
-            </p>
-            <p id="contact-me">
-                Please navigate around and checkout out some of my projects!  My contact information is below.
-            </p>
+        <section className="about-container">
+            <img src={coverImage} className="profile-img" alt="cover" />
+            <div className="about-me">
+                <p className="salutation">
+                    Hi!
+                </p>
+                <p id="profile-overview">
+                    I'm a product manager that focuses on new product development, and an aspiring full-stack web developer. I love technology and I'm always looking to learn new ways to do things.
+                </p>
+                <p className="contact-me">
+                    Please navigate around and check out out some of my projects!  Click on 'Contact' to connect.
+                </p>
+                <img src={heroImage} className="hero-img" alt="MacBook Hero" />
+            </div>
         </section>
     );
 }
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import logo from '../../assets/images/main-logo.png';
 
-function Nav () {
-    
+function Nav() {
+
     return (
         <header>
             <h2 className="nav-bar">
                 <a href="/">
                     <img src={logo} alt="logo" className="site-logo" />
-                </a> 
+                </a>
+                <nav>
+                    <ul className="flex-row">
+                        <li className="mx-2">
+                            <a href="#about">
+                                About Me
+                            </a>
+                        </li>
+                        <li>
+                            <span>Projects</span>
+                        </li>
+                        <li>
+                            <span>Contact</span>
+                        </li>
+                        <li>
+                            <span>Resume</span>
+                        </li>
+                    </ul>
+                </nav>
             </h2>
-            <nav>
-                <ul className="flex-row">
-                    <li className="mx-2">
-                        <a href="#about">
-                            About Me
-                        </a>
-                    </li>
-                    <li>
-                        <span>Projects</span>
-                    </li>
-                    <li>
-                        <span>Contact</span>
-                    </li>
-                    <li>
-                        <span>Resume</span>
-                    </li>
-                </ul>
-            </nav>
         </header>
     );
 }
